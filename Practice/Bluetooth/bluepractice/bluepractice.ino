464d5124d226d33e27fec9494c77676dde241cca
KidanJIN <ihansam@skku.edu>
2020-05-08 14:22:12 +0900

    make my branch, start APP develop

--- Practice/Bluetooth/bluepractice/bluepractice.ino
+++ Practice/Bluetooth/bluepractice/bluepractice.ino
@@ -1,14 +1,9 @@
 #include <SoftwareSerial.h>
- 
-/**
- *  RX_PIN은 HC 모듈의 TX 핀과 연결될 핀
- *  TX_PIN은 HC 모듈의 RX 핀과 연결될 핀
- */
-#define RX_PIN 3
-#define TX_PIN 2
+#define RX_PIN 3          // HC의 TX에 연결
+#define TX_PIN 2          // HC의 RX에 연결
 #define SERIAL_BAUD 9600
 #define HC_BAUD 9600
- 
+
 SoftwareSerial HCSerial(RX_PIN, TX_PIN);
  
 void setup() {
@@ -17,30 +12,24 @@ void setup() {
 }
  
 void loop() {
-  if(HCSerial.available()) {
-    // HC 모듈로부터 데이터가 들어온 경우 수행 루틴
+  if(HCSerial.available()) {  // HC로 데이터가 들어올 때
     HCFunction();
   }
-  if(Serial.available()) {
-    // 시리얼 모니터로부터 데이터가 들어온 경우 수행 루틴
+  if(Serial.available()) {    // 모니터로 데이터가 들어올 때
     SerialFunction();
   }
 }
  
 void HCFunction() {
-  /** 
-   * Init routine
-   * HC 모듈로부터 들어온 데이터를 시리얼 모니터로 출력 
-   * 커스터마이징할 함수 영역
+  /*
+   * 시리얼 모니터에 출력
    */
   Serial.write(HCSerial.read());
 }
  
 void SerialFunction() {
-  /**
-   * Init routine
-   * 시리얼 모니터로부터 들어온 데이터를 HC 모듈로 출력
-   * 커스터마이징할 함수 영역
+  /*
+   * 블루투스 모듈에 출력
    */
   HCSerial.write(Serial.read());
 }
