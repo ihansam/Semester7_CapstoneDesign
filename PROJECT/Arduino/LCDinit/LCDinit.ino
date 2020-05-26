3341ac97599b18ce1c132f9eaaad97021425b36e
KidanJIN <ihansam@skku.edu>
2020-05-26 23:49:44 +0900

    modify directories

---
+++ PROJECT/Arduino/LCDinit/LCDinit.ino
@@ -0,0 +1,12 @@
+#include <LiquidCrystal_I2C.h>    // LCD 라이브러리
+LiquidCrystal_I2C lcd(0x27,16,2);
+
+void setup() {
+  // put your setup code here, to run once:
+  lcd.init();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
