7e979f02da0658df9d055112a3da04bc4d6f59da
KidanJIN <ihansam@skku.edu>
2020-06-01 23:04:39 +0900

    DEBUG1 complete

--- PROJECT/Arduino/main/mainDebug1/mainDebug1.ino
+++ PROJECT/Arduino/main/mainDebug1/mainDebug1.ino
@@ -10,11 +10,11 @@
 // 마지막으로 이미지센서 ->허리 굽음 
 // 총 5개의 자세로 분류함
 
-// 라이브러리 및 객체 선언
+// 라이브러리 및 객체 선언 ----------------------------------------------
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27,16,2);
 
-// 핀 번호 할당
+// 핀 번호 할당 ---------------------------------------------------------
 //const int L1 = A0;              // 앞
 //const int R1 = A1;
 #define L2 A2                     // 압력 센서 핀
@@ -24,12 +24,12 @@ LiquidCrystal_I2C lcd(0x27,16,2);
 #define TRIG 2                    // 초음파 센서 핀
 #define ECHO 3
 #define redPin 9                  // 실시간 알림용 led 핀
+#define greenPin 10
 #define bluePin 11   
-#define greenPin 10 
 #define btn_onoff 5               // 실시간 LED 알림 onoff 버튼 (흰색)
 #define btn_capture 4             // 실시간 자세 LCD 확인 버튼 (검정색)
 
-// Parameters
+// Parameters ------------------------------------------------------------
 // 자세의 개수 및 이름
 const int posnum = 5;
 enum POSTURES {PROPER=0, HIP_FRONT, BEND_LEFT, BEND_RIGHT, BACK_CURVED};
@@ -40,16 +40,24 @@ String pos[posnum] = {"  Good Posture! ", " Far from Back! ", "   Bend Left!   "
 const int distanceSN = 4;         // 엉덩이가 떨어져 앉은 것으로 판단하는 초음파 센서 거리 기준
 const int notBalanceDiff = 400;   // 좌우 불균형으로 판단하는 압력 센서 차이 값 기준
 const int sstime = 10;            // 자세 판별 간격: 1초
+const int maxDATA = 10;           // 데이터가 이만큼 모이면 데이터 분석 및 어플로 REPORT 전송
+const int stretchTime = 200;      // 스트레칭 알림 주기 (20초 이상 오래 앉아있을 시 알림)
+const int ledDuration = 500;      // led 점멸 시간 간격
+const int loopPeriod = 100;       // main loop 간격
 
-// Grobal Variables
+// Grobal Variables --------------------------------------------------------
 bool btn_onoff_pushed = false;    // LED 알림 onoff 버튼이 이전 clock에 눌렸었는지
 bool btn_capture_pushed = false;  // LCD 표시 버튼이 이전 clock에 눌렸었는지
 int isRealtimeON = 1;             // 실시간 LED 알림 모드 ON/OFF 상태 (초기 1)
-int sitTime = 0;                  // 앉은 누적 시간 측정 (자세가 바르던 나쁘던 앉기만하면됨 = FSR에 뭔가 감지되기만 하면)
 int sscnt = 0;                    // 매 loop마다 count하여 sstime이 되면 자세 판별
 int currPos = PROPER;             // 현재 자세
+int posDATA[posnum] = {0,};       // 판단 자세 결과 누적 배열 (각 인덱스는 enum POSTURES의 index와 일치)
+int numDATA = 0;                  // posDATA에 모인 데이터 개수
+int sitTime = 0;                  // 앉은 누적 시간 측정 (자세가 바르던 나쁘던 앉기만하면됨 = FSR에 뭔가 감지되기만 하면)
+unsigned long prevLEDtime;        // led 점멸을 위한 시간 측정
+bool isLEDon = false;             // 점멸하는 led의 onoff 상태
 
-// FUCNTIONS
+// FUCNTIONS --------------------------------------------------------------
 bool checkSIT(int l, int r){          // NEEDMODIFY
   if (l>=450 && l<550 && r>=450 && r<550)
     return false;
@@ -86,7 +94,19 @@ int sideBalance(int L, int R) {
   }
 }
 
-// MAIN
+bool checkBackBend(){
+  // 라즈베리파이로부터 현재 이미지 판독 결과를 받아오는 부분
+  // if (^^) return true;
+  return false;
+}
+
+void RGBWrite(int r, int g, int b){
+  analogWrite(redPin, r);
+  analogWrite(greenPin, g);
+  analogWrite(bluePin, b);
+}
+
+// MAIN =========================================================================
 void setup()
 {
   Serial.begin(9600);
@@ -104,6 +124,8 @@ void setup()
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print(RTonoff[isRealtimeON]);
+
+  RGBWrite(0,0,0);
 }
 
 void loop()
@@ -158,15 +180,15 @@ void loop()
     }      
   }
 
-  // 자세 판단 부분 --------------------------------------------
+  // 자세 판단 부분 --------------------------------------------------------------
   if(isSIT){
     if(sscnt < sstime) ++sscnt;
     else{                                         // 자세 판단 시간이 되었을 때
       sscnt = 0;
-      if(UltraSonic() > distanceSN)               // 1. 엉덩이를 붙이지 않은 자세
-        currPos = HIP_FRONT;
+      if(UltraSonic() > distanceSN)               // 초음파 센서 체크
+        currPos = HIP_FRONT;                      // 1. 엉덩이를 붙이지 않은 자세
       else{ 
-        int checkBalance = sideBalance(valL, valR);
+        int checkBalance = sideBalance(valL, valR);   // 압력 센서 체크
         if (checkBalance == 2){
           currPos = BEND_LEFT;                    // 2. 왼쪽으로 기운 자세
         }
@@ -174,17 +196,48 @@ void loop()
           currPos = BEND_RIGHT;                   // 3. 오른쪽으로 기운 자세
         }
         else{
-          // 라즈베리
-          currPos = PROPER;                       // 0. 바른 자세
+          if (checkBackBend())                    /// 이미지 센서 체크
+            currPos = BACK_CURVED;                // 4. 허리가 굽은 자세
+          else currPos = PROPER;                  // 0. 바른 자세
         }
       }
-      Serial.println("==================현재 자세==================");
+      posDATA[currPos] += 1;                      // 판단 자세 결과를 posDATA에 누적
+      ++numDATA;
+      Serial.println("==================현재 자세==================");      // DEBUG
       Serial.println(pos[currPos]);
       Serial.println("=============================================");
     }
   }    
 
-  delay(100);
+  // RGB LED 출력 부분 -----------------------------------------------------------
+  unsigned long currentTime = millis();
+  if (sitTime == 0) RGBWrite(0,0,0);              // 앉아있지 않으면 LED off
+  else if (sitTime == stretchTime){               // 스트레칭 알림이 실시간 알림보다 우선
+    //StretchingAlarm();
+  }
+  else if (sitTime > stretchTime){
+    if(currentTime > prevLEDtime + ledDuration){
+      prevLEDtime = currentTime;
+      isLEDon = !isLEDon;
+      RGBWrite(255*isLEDon, 255*isLEDon, 0);      // isLEDon이 true면 ON, false면 OFF
+    }
+  }
+  else{                                           // 스트레칭 시간이 아니면 실시간 알림
+    if(isRealtimeON && isSIT){                    // 실시간 알림이 ON이고, 앉아있을 때만 동작
+      if(currPos) RGBWrite(255, 0, 0);            // currPos가 0이 아니면 나쁜 자세이므로, 빨간색
+      else RGBWrite(0, 255, 0);                   // 0이면 바른 자세이므로, 초록색
+    }
+    else RGBWrite(0,0,0);
+  }
+  
+  // 자세 데이터가 일정 개수 모이면 분석 후 REPORT를 생성하고 어플에 보내는 부분
+  if(numDATA >= maxDATA){
+    // ShowReport(posDATA, posnum);
+    numDATA = 0;
+    for (int i=0; i<posnum; ++i) posDATA[i]=0;    // posDATA 초기화
+  }
+
+  delay(loopPeriod);
 }
 
 
